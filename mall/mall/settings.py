3a64b1159f08076c0b812b9b0ea32abea1db7997
waikau on ubuntu home <waikaumaina1@gmail.com>
2021-05-16 23:39:33 +0300

    created the registration form

--- mall/mall/settings.py
+++ mall/mall/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'shop',
-    'authenticate',
+    'users',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
