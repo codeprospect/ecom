ad900c78d21166d251d3464de5c796e13fb790ae
SteveSpec <wambuastephen001@gmail.com>
2021-05-18 16:29:06 +0300

    Added login, logout and profile standard templates

--- mall/users/views.py
+++ mall/users/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import UserRegisterForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -17,3 +18,33 @@ def register(request):
         form = UserRegisterForm()
 
     return render(request, 'users/registration.html', {'form': form})
+
+#login view
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
+
+# def login(request):
+#     if request.method == "POST":
+# 		form = AuthenticationForm(request, data=request.POST)
+# 		if form.is_valid():
+# 			username = form.cleaned_data.get('username')
+# 			password = form.cleaned_data.get('password')
+# 			user = authenticate(username=username, password=password)
+# 			if user is not None:
+# 				login(request, user)
+# 				messages.info(request, f"You are now logged in as {username}.")
+# 				return redirect('homepage')
+# 		else:
+# 			messages.error(request,"Invalid username or password.")
+# 	else:
+# 		messages.error(request,"Invalid username or password.")
+#
+#     form = AuthenticationForm()
+#     return render(request, 'users/login.html')
+#
+#
+# #logout view
+# def logout(request):
+#     return render(request, 'users/logout.html')
