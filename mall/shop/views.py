53f535024d597d82f4dbd55b0499e32c84c2a125
SteveSpec <wambuastephen001@gmail.com>
2021-05-18 12:06:32 +0300

    Added minor comments

--- mall/shop/views.py
+++ mall/shop/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 
-# steve kindly Create your views here.
+# steve kindly Create your views here. And comment them every step
 def homepage(request):
     return render(request, 'shop/home.html')
