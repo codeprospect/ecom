79731cae0ee3f2390e62bf5cfabfb49fec59e672
waikau windows <waikaumaina1@gmail.com>
2021-07-09 16:31:49 +0300

    created the product model and a test for it

--- mall/shop/models.py
+++ mall/shop/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Product(models.Model):
+    product_name = models.CharField(max_length=30)
+    product_price = models.CharField(max_length=30)
+    product_quantity = models.IntegerField()
+
+    def __str__(self):
+        return self.product_name
--- mall/shop/tests.py
+++ mall/shop/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from .models import Product
 
 # Create your tests here.
+
+
+class ProductModelTests(TestCase):
+
+    def product_was_created_successfully(self):
+        new_product = Product()
